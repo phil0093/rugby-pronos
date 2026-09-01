305f0059948edaeecc68aa5ee700361c7921f390
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-01 14:58:21 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
-console.log("FIREBASE JS CHARGE");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxZ-e7pbvgfxPGnSFcgr2RQOn_0XFnU5M",
+    authDomain: "rugby-pronos.firebaseapp.com",
+    projectId: "rugby-pronos",
+    storageBucket: "rugby-pronos.firebasestorage.app",
+    messagingSenderId: "367460767993",
+    appId: "1:367460767993:web:4735c83e703b359e1d2a28"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
