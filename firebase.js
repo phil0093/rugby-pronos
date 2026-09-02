b4e104c7557fc84ea0ca1a76825b655a49483a56
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-02 07:50:19 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,20 +5,18 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxZ-e7pbvgfxPGnSFcgr2RQOn_0XFnU5M",
-  authDomain: "rugby-pronos.firebaseapp.com",
-  projectId: "rugby-pronos",
-  storageBucket: "rugby-pronos.firebasestorage.app",
-  messagingSenderId: "367460767993",
-  appId: "1:367460767993:web:4735c83e703b359e1d2a28"
+    apiKey: "AIzaSyCxZ-e7pbvgfxPGnSFcgr2RQOn_0XFnU5M",
+    authDomain: "rugby-pronos.firebaseapp.com",
+    projectId: "rugby-pronos",
+    storageBucket: "rugby-pronos.firebasestorage.app",
+    messagingSenderId: "367460767993",
+    appId: "1:367460767993:web:4735c83e703b359e1d2a28"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-window.db = db;
+export { db };
 
 console.log("Firebase connecté");
-console.log(window.db);
-`
